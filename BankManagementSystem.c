850070f5cc601ebbe8c0b29d81216b7e18cc5d8f
himansh <himansh>
2023-03-21 15:45:02 +0530

    add customerfunction

--- BankManagementSystem.c
+++ BankManagementSystem.c
@@ -6,7 +6,17 @@ struct customer
     int account_number;
     float balance;
 };
-
+void add_customer(struct customer *customers, int *count)
+{
+    printf("Enter customer name: ");
+    scanf("%s", customers[*count].name);
+    printf("Enter account number: ");
+    scanf("%d", &customers[*count].account_number);
+    printf("Enter initial balance: ");
+    scanf("%f", &customers[*count].balance);
+    (*count)++;
+    printf("Customer added successfully!\n");
+}
 
 
 int main(){
